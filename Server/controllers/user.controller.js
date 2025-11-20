5db3bbff6830e4f05da2c814bcb45f8e702d1ed0
Mohammed Galal <muhammed.abdallah@Student.giu-uni.de>
2025-11-20 15:57:14 +0200

    "Added manager routes, controller, service. models as empty files. no implementation yet"

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -3,7 +3,7 @@ const userService = require("../services/user.service");
 
 const userController = {
 
-    deleteAccount: async (req, res) => {
+    deleteAccount: async (req, res) => { //can manager also do it?
         try {
             const { id } = req.params;
 
@@ -29,7 +29,7 @@ const userController = {
         }
     },
 
-    getCurrentUser: async (req, res) => {
+    getCurrentUser: async (req, res) => { //can manager also do it?
         try {
             const {id} = req.user;
             //call service
@@ -60,7 +60,7 @@ const userController = {
         }
     },
 
-    updateUserProfile: async (req, res) => {
+    updateUserProfile: async (req, res) => { //can manager also do it?
         try {
             const {id} = req.user;
             const {name, email, password} = req.body;
@@ -88,7 +88,7 @@ const userController = {
         }   
     },
 
-    getLogs: async (req, res) => {
+    getLogs: async (req, res) => { //what does this do?
         try {
             const {id} = req.params;
             //call service
